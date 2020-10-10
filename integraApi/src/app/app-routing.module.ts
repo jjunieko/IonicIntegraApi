106dadb7d5bcd662543d47e0dc896f9599131b9b
joao junior <juniorgtr123@gmail.com>
2020-10-09 21:45:46 -0300

    update api service

---
+++ integraApi/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "folder/page1",
+    pathMatch: "full",
+  },
+  {
+    path: "folder/:id",
+    loadChildren: () =>
+      import("./folder/folder.module").then((m) => m.FolderPageModule),
+  },
+  {
+    path: "page1",
+    loadChildren: () =>
+      import("./page/page1/page1.module").then((m) => m.Page1PageModule),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
